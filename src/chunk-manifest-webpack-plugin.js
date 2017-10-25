0e01a4f40541ad2f1aa72a10427ae7200e6e730f
Jouni Kantola <jouni@kantola.se>
2017-10-26 00:01:46 +0200

    Refactor plugin to ES2015 class

--- src/chunk-manifest-webpack-plugin.js
+++ src/chunk-manifest-webpack-plugin.js
@@ -1,70 +1,74 @@
+"use strict";
+
 var RawSource = require("webpack-core/lib/RawSource");
 
-function ChunkManifestPlugin(options) {
-  options = options || {};
-  this.manifestFilename = options.filename || "manifest.json";
-  this.manifestVariable = options.manifestVariable || "webpackManifest";
-}
-module.exports = ChunkManifestPlugin;
+class ChunkManifestPlugin {
+  constructor(options) {
+    options = options || {};
+    this.manifestFilename = options.filename || "manifest.json";
+    this.manifestVariable = options.manifestVariable || "webpackManifest";
+  }
 
-ChunkManifestPlugin.prototype.constructor = ChunkManifestPlugin;
-ChunkManifestPlugin.prototype.apply = function(compiler) {
-  var manifestFilename = this.manifestFilename;
-  var manifestVariable = this.manifestVariable;
-  var oldChunkFilename;
-  var chunkManifest;
+  apply(compiler) {
+    var manifestFilename = this.manifestFilename;
+    var manifestVariable = this.manifestVariable;
+    var oldChunkFilename;
+    var chunkManifest;
 
-  compiler.plugin("this-compilation", function(compilation) {
-    var mainTemplate = compilation.mainTemplate;
-    mainTemplate.plugin("require-ensure", function(_, chunk, hash) {
-      var filename =
-        this.outputOptions.chunkFilename || this.outputOptions.filename;
+    compiler.plugin("this-compilation", function(compilation) {
+      var mainTemplate = compilation.mainTemplate;
+      mainTemplate.plugin("require-ensure", function(_, chunk, hash) {
+        var filename =
+          this.outputOptions.chunkFilename || this.outputOptions.filename;
 
-      if (filename) {
-        chunkManifest = [chunk].reduce(function registerChunk(manifest, c) {
-          if (c.id in manifest) return manifest;
-          var hasRuntime = typeof c.hasRuntime === "function"
-            ? c.hasRuntime()
-            : c.entry;
-          if (hasRuntime) {
-            manifest[c.id] = undefined;
-          } else {
-            manifest[
-              c.id
-            ] = mainTemplate.applyPluginsWaterfall("asset-path", filename, {
-              hash: hash,
-              chunk: c
-            });
-          }
-          return c.chunks.reduce(registerChunk, manifest);
-        }, {});
-        oldChunkFilename = this.outputOptions.chunkFilename;
-        this.outputOptions.chunkFilename = "__CHUNK_MANIFEST__";
-        // mark as asset for emitting
-        compilation.assets[manifestFilename] = new RawSource(
-          JSON.stringify(chunkManifest)
+        if (filename) {
+          chunkManifest = [chunk].reduce(function registerChunk(manifest, c) {
+            if (c.id in manifest) return manifest;
+            var hasRuntime = typeof c.hasRuntime === "function"
+              ? c.hasRuntime()
+              : c.entry;
+            if (hasRuntime) {
+              manifest[c.id] = undefined;
+            } else {
+              manifest[
+                c.id
+              ] = mainTemplate.applyPluginsWaterfall("asset-path", filename, {
+                hash: hash,
+                chunk: c
+              });
+            }
+            return c.chunks.reduce(registerChunk, manifest);
+          }, {});
+          oldChunkFilename = this.outputOptions.chunkFilename;
+          this.outputOptions.chunkFilename = "__CHUNK_MANIFEST__";
+          // mark as asset for emitting
+          compilation.assets[manifestFilename] = new RawSource(
+            JSON.stringify(chunkManifest)
+          );
+        }
+
+        return _;
+      });
+    });
+
+    compiler.plugin("compilation", function(compilation) {
+      compilation.mainTemplate.plugin("require-ensure", function(
+        _,
+        chunk,
+        hash,
+        chunkIdVar
+      ) {
+        if (oldChunkFilename) {
+          this.outputOptions.chunkFilename = oldChunkFilename;
+        }
+
+        return _.replace(
+          '"__CHUNK_MANIFEST__"',
+          'window["' + manifestVariable + '"][' + chunkIdVar + "]"
         );
-      }
-
-      return _;
+      });
     });
-  });
+  }
+}
 
-  compiler.plugin("compilation", function(compilation) {
-    compilation.mainTemplate.plugin("require-ensure", function(
-      _,
-      chunk,
-      hash,
-      chunkIdVar
-    ) {
-      if (oldChunkFilename) {
-        this.outputOptions.chunkFilename = oldChunkFilename;
-      }
-
-      return _.replace(
-        '"__CHUNK_MANIFEST__"',
-        'window["' + manifestVariable + '"][' + chunkIdVar + "]"
-      );
-    });
-  });
-};
+module.exports = ChunkManifestPlugin;
