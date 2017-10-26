54520ef4ffc8cdd0bf95fe367f7b9f28746e69b6
Jouni Kantola <jouni@kantola.se>
2017-10-27 01:31:41 +0200

    Replace runtime's chunk manifest with lookup

---
+++ test/plugin-runtime-replacement-test.js
@@ -0,0 +1,52 @@
+import test from "ava";
+
+import ChunkManifestPlugin from "../src/chunk-manifest-webpack-plugin";
+
+test.cb("replace runtime's chunk manifest with lookup", t => {
+  const chunkIdVariableName = "a-chunk-id-variable";
+  const manifestVariable = "manifest-variable";
+  const placeholder = `"__CHUNK_MANIFEST__"`;
+
+  const runtimeSource = [
+    "runtime-source-start",
+    placeholder,
+    ";",
+    "runtime-source-end"
+  ];
+
+  let expected = [...runtimeSource];
+  expected[1] = `window["${manifestVariable}"][${chunkIdVariableName}]`;
+
+  const compilationPluginEvent = (compilationEvent, ensure) => {
+    if (compilationEvent === "require-ensure") {
+      const updatedSource = ensure(
+        runtimeSource.join(""),
+        undefined,
+        undefined,
+        chunkIdVariableName
+      );
+      t.is(updatedSource, expected.join(""));
+      t.end();
+    }
+  };
+
+  const pluginEvent = (event, compile) => {
+    if (event === "compilation") {
+      const compilation = {
+        mainTemplate: {
+          plugin: compilationPluginEvent
+        }
+      };
+      compile(compilation);
+    }
+  };
+
+  const fakeCompiler = { plugin: pluginEvent };
+
+  const plugin = new ChunkManifestPlugin({
+    manifestVariable
+  });
+
+  plugin.plugins = [{ apply: () => {} }];
+  plugin.apply(fakeCompiler);
+});
